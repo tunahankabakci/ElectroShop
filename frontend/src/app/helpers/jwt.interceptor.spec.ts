a2aac9152cae21f682081ff1352243fe7e00c9e7
tunahankabakci <l1411008056@stud.sdu.edu.tr>
2020-02-26 07:34:52 +0300

    jwt interceptor bugfix

--- frontend/src/app/helpers/jwt.interceptor.spec.ts
+++ frontend/src/app/helpers/jwt.interceptor.spec.ts
@@ -1,25 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthenticationService } from '../services/authentication.service';
+import { AuthService } from '../services/auth.service';
 
 
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
-
+    constructor(private injector: Injector) { }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add authorization header with jwt token if available
-        let currentUser = this.authenticationService.currentUserValue;
-        if (currentUser && currentUser.token) {
-            request = request.clone({
-                setHeaders: { 
-                    Authorization: `Bearer ${currentUser.token}`
-                }
-            });
+        var authService = this.injector.get(AuthService)
+        var token = authService.token
+
+        if (token) {
+            request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
         }
 
-        return next.handle(request);
+        if (!request.headers.has('Content-Type')) {
+            request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
+        }
+
+        request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
+
+        return next.handle(request)
     }
 }
